93cd52832b1a8b706948abf06a7002c42ba078ee
trevape <unknown995890@gmail.com>
2026-07-23 16:36:03 +0200

    Add notification click event listener in sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function(clientList) {
+            if (clientList.length > 0) {
+                return clientList[0].focus();
+            }
+            return clients.openWindow('/');
+        })
+    );
+});
+
